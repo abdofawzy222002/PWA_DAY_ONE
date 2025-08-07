4a939f287a423a5e7f02ad0f046ea7557f000e2b
Abdelrahman Fawzy <abdelrahmanfawzy185@gmail.com>
2025-08-08 00:54:06 +0300

    EDIT3

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
   msg.style.color = "yellow"; 
   main.appendChild(msg);
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('Service Worker registered', reg))
+    .catch(err => console.error('Service Worker registration failed:', err));
+}
